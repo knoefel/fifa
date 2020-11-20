d810ab884935193e7c1dce4f5a689c762b21b6f1
Knöfel Andreas <andreas.knoefel@ti8m.ch>
2020-11-20 13:53:23 +0100

    fix(header): add header tag

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,16 +1,18 @@
 <template>
-  <v-row class="header align-center justify-space-between justify-lg-end flex-grow-0 px-6 px-lg-12" no-gutters>
-    <v-col class="flex-grow-0">
-      <v-icon class="icon">mdi-menu</v-icon>
-    </v-col>
-    <v-col class="flex-grow-0 ml-lg-6 mr-lg-auto">
-      <v-img class="logo" :src="require(`@/assets/fifa-logo.svg`)"></v-img>
-    </v-col>
+  <header>
+    <v-row class="header align-center justify-space-between justify-lg-end flex-grow-0 px-6 px-lg-12" no-gutters>
+      <v-col class="flex-grow-0">
+        <v-icon class="icon">mdi-menu</v-icon>
+      </v-col>
+      <v-col class="flex-grow-0 ml-lg-6 mr-lg-auto">
+        <v-img class="logo" :src="require(`@/assets/fifa-logo.svg`)"></v-img>
+      </v-col>
 
-    <v-col class="flex-grow-0">
-      <v-icon class="icon">mdi-account-outline</v-icon>
-    </v-col>
-  </v-row>
+      <v-col class="flex-grow-0">
+        <v-icon class="icon">mdi-account-outline</v-icon>
+      </v-col>
+    </v-row>
+  </header>
 </template>
 
 <script>
